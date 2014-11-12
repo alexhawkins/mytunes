d6328c10227cdb7a8304ea741f61a66109340f00
Alex Hawkins <alexhawkins.me@gmail.com>
2014-11-12 15:45:00 -0800

    create SongQueue view

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,13 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
+    this.on('add', function(){
+      this.playFirst();
+    })
+  },
+
+  playFirst: function(){
+    console.log(this);
   }
 
 });
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,11 +1,25 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
+  tagName: "table",
+
+  template: _.template('<th><strong>Queue</strong></th>'),
+
   initialize: function() {
+    this.collection.on('add', this.render, this);
   },
 
   render: function() {
-    return this.$el;
+     // to preserve event handlers on child nodes, we must call .detach() on them before overwriting with .html()
+    // see http://api.jquery.com/detach/
+    this.$el.children().detach();
+
+    this.$el.html(this.template).append(
+      this.collection.map(function(song){
+        return new SongQueueEntryView({model: song}).render();
+      })
+    );
+
   }
 
 });
