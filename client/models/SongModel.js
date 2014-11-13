b1318b41dcdd866d53787763f55c31e69d8aa9b5
Alex Hawkins <alexhawkins.me@gmail.com>
2014-11-12 21:26:49 -0800

    Fix typo

--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -1,39 +1,39 @@
 // SongModel.js - Defines a backbone model class for songs.
 var SongModel = Backbone.Model.extend({
 
-  initialize: function(){
+  initialize: function() {
     this.set('playCounter', 0);
     this.set('upvotes', 0);
     this.set('downvotes', 0);
     this.set('voteTotal', 0);
   },
 
-  play: function(){
+  play: function() {
     // Triggering an event here will also trigger the event on the collection
     this.trigger('play', this);
   },
-  enqueue: function(){
+  enqueue: function() {
     // add song to our queue
     this.trigger('enqueue', this);
   },
-    // remove song from our queue
-  dequeue: function(){
-     this.trigger('dequeue', this);
+  // remove song from our queue
+  dequeue: function() {
+    this.trigger('dequeue', this);
   },
-    // let our app know that the song is over
-  ended: function(){
+  // let our app know that the song is over
+  ended: function() {
     this.trigger('ended', this);
     this.set('playCounter', this.get('playCounter') + 1);
   },
 
-  upvote: function(){
+  upvote: function() {
     this.set('upvotes', this.get('upvotes') + 1);
     this.set('voteTotal', this.get('downvotes') + this.get('upvotes'));
   },
 
-  downvote: function(){
+  downvote: function() {
     this.set('downvotes', this.get('downvotes') - 1);
     this.set('voteTotal', this.get('downvotes') + this.get('upvotes'));
-  },
+  }
 
 });
